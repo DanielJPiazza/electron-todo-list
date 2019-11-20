398d46bdb59a59da0b8c85d89f294e682ffbb007
DanielJPiazza <42113905+DanielJPiazza@users.noreply.github.com>
2019-11-20 18:57:38 -0500

    Initial file upload and commit.
    Create basic to-do list functionality.

---
+++ todo.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// -- INITIAL SETUP --
+
+// Create and append close button to each existing list item.
+var nodes = document.getElementsByTagName('LI');
+
+for (var i = 0; i < nodes.length; i++) {
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode('\u00D7');
+    span.className = 'closeButton';
+    span.appendChild(txt);
+    nodes[i].appendChild(span);
+}
+
+// Toggle strikethrough on click for existing list items.
+var list = document.querySelector('UL');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName == 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+// Add remove node event to existing remove task buttons.
+var remove = document.getElementsByTagName('LI');
+for (var i = 0; i < remove.length; i++) {
+    remove[i].addEventListener('click', function(ev) {
+        if (ev.target.className == 'closeButton') {
+            ev.target.parentNode.remove();
+        }
+    }, false);
+}
+
+// -- MAIN NEW TASK FUNCTION --
+
+// Create a new list item when 'Add Task' is clicked.
+function createTask() {
+    var li = document.createElement('LI');
+    var inputValue = document.getElementById('newTaskInput').value;
+    var txt = document.createTextNode(inputValue);
+    li.appendChild(txt);
+
+    // Toggle strikethrough on click for new list item.
+    txt.addEventListener('click', function(ev) {
+        ev.target.classList.toggle('checked');
+    });
+    
+    // Append new item to task list.
+    document.querySelector('UL').appendChild(li);
+    
+    // Clear add task field.
+    document.getElementById('newTaskInput').value = '';
+
+    // Append close button and add remove node event to new item.
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode('\u00D7');
+    span.className = 'closeButton';
+    span.appendChild(txt);
+    span.addEventListener('click', function(ev) {
+        ev.target.parentNode.remove();
+    });
+    li.appendChild(span);
+}
