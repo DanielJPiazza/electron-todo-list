7b9ba4e74533084b9ee3eae56175f1c9c242b62f
DanielJPiazza <daniel@example.com>
2019-12-02 09:40:54 -0500

    Add event listener for 'ENTER' key to 'ADD TASK' button.

--- todo.js
+++ todo.js
@@ -21,6 +21,13 @@ function setFocusAddTaskField(){
 
 // -- INITIAL SETUP --
 
+// Add event listener to 'ADD TASK' button for 'ENTER' key.
+document.getElementById('newTaskInput').addEventListener('keypress', function(e) {
+    if (e.which === 13 || e.keyCode === 13) {
+      createTask();
+    }
+}, false);
+
 // Create and append close button to each existing list item.
 var nodes = document.getElementsByTagName('LI');
 
@@ -33,19 +40,19 @@ for (var i = 0; i < nodes.length; i++) {
 }
 
 // Toggle strikethrough on click for existing list items.
-var list = document.querySelector('UL');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName == 'LI') {
-        ev.target.classList.toggle('checked');
+document.querySelector('UL').addEventListener('click', function(e) {
+    if (e.target.tagName == 'LI') {
+        e.target.classList.toggle('checked');
     }
 }, false);
 
 // Add remove node event to existing remove task buttons.
 var remove = document.getElementsByTagName('LI');
+
 for (var i = 0; i < remove.length; i++) {
-    remove[i].addEventListener('click', function(ev) {
-        if (ev.target.className == closeButtonClasses) {
-            ev.target.parentNode.remove();
+    remove[i].addEventListener('click', function(e) {
+        if (e.target.className == closeButtonClasses) {
+            e.target.parentNode.remove();
         }
     }, false);
 }
@@ -61,8 +68,8 @@ function createTask() {
     li.appendChild(txt);
 
     // Toggle strikethrough on click for new list item.
-    txt.addEventListener('click', function(ev) {
-        ev.target.classList.toggle('checked');
+    txt.addEventListener('click', function(e) {
+        e.target.classList.toggle('checked');
     });
     
     // Append new item to task list.
@@ -76,8 +83,8 @@ function createTask() {
     var txt = document.createTextNode(closeButton);
     span.className = closeButtonClasses;
     span.appendChild(txt);
-    span.addEventListener('click', function(ev) {
-        ev.target.parentNode.remove();
+    span.addEventListener('click', function(e) {
+        e.target.parentNode.remove();
     });
     li.appendChild(span);
 
