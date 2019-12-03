45f7b697770c4ccdddfad725d3e3f7da28e8a199
DanielJPiazza <daniel@example.com>
2019-12-02 19:28:02 -0500

    Add handling for whitespace-only tasks.
    General styling.
    
    Comment cleanup and var organization.

--- todo.js
+++ todo.js
@@ -1,36 +1,40 @@
 'use strict';
 
-// -- GLOBAL VARIABLES --
+// -- IMPORTS --
+const remote = require('electron').remote
 
-var closeButtonClasses = 'closeButton';
-var closeButton = '\u2716';
+
+// -- GLOBAL VARIABLES --
+const closeButtonClasses = 'closeButton';
+const closeButton = '\u2716';
+const inputErrorMessage = document.getElementById('inputErrorMessage');
+const addTaskButton = document.getElementById('newTaskInput');
 
 
 // -- UTILITY FUNCTIONS --
-
 function closeWindow() {
-    const remote = require('electron').remote
-    let w = remote.getCurrentWindow()
-    w.close()
+    // Not used in todo.js. Called in index.html.
+    remote.getCurrentWindow().close()
 }
 
 function setFocusAddTaskField(){
-    document.getElementById("newTaskInput").focus();
+    document.getElementById('newTaskInput').focus();
+}
+
+function openErrorMessage(message) {
+    document.getElementById('inputErrorMessage').textContent = message;
+    inputErrorMessage.style.display = 'inline-block';
+}
+
+function closeErrorMessage() {
+    document.getElementById('inputErrorMessage').textContent = '';
+    inputErrorMessage.style.display = 'none';
 }
 
 
 // -- INITIAL SETUP --
-
-// Add event listener to 'ADD TASK' button for 'ENTER' key.
-document.getElementById('newTaskInput').addEventListener('keypress', function(e) {
-    if (e.which === 13 || e.keyCode === 13) {
-      createTask();
-    }
-}, false);
-
 // Create and append close button to each existing list item.
 var nodes = document.getElementsByTagName('LI');
-
 for (var i = 0; i < nodes.length; i++) {
     var span = document.createElement('SPAN');
     var txt = document.createTextNode(closeButton);
@@ -39,16 +43,8 @@ for (var i = 0; i < nodes.length; i++) {
     nodes[i].appendChild(span);
 }
 
-// Toggle strikethrough on click for existing list items.
-document.querySelector('UL').addEventListener('click', function(e) {
-    if (e.target.tagName == 'LI') {
-        e.target.classList.toggle('checked');
-    }
-}, false);
-
 // Add remove node event to existing remove task buttons.
 var remove = document.getElementsByTagName('LI');
-
 for (var i = 0; i < remove.length; i++) {
     remove[i].addEventListener('click', function(e) {
         if (e.target.className == closeButtonClasses) {
@@ -57,30 +53,61 @@ for (var i = 0; i < remove.length; i++) {
     }, false);
 }
 
+// Toggle 'checked' class on click for existing list items.
+document.querySelector('UL').addEventListener('click', function(e) {
+    if (e.target.tagName == 'LI') {
+        e.target.classList.toggle('checked');
+    }
+}, false);
+
+// Allow 'Enter' key to call createTask().
+addTaskButton.addEventListener('keypress', function(e) {
+    if (e.which === 13 || e.keyCode === 13) {
+      createTask();
+    }
+}, false);
+
 
 // -- NEW TASK FUNCTION --
-
 // Create a new list item when 'Add Task' is clicked.
 function createTask() {
+    // Clear any displayed error messages.
+    closeErrorMessage();
+    
+    // Gather user input.
+    var inputValue = document.getElementById('newTaskInput').value.trim();
+
+    // Verify user input contains characters other than whitepsace.
+    var inputRegex = new RegExp('^\\S');
+    if (!inputRegex.test(inputValue)) {
+        // If check fails, display error message.
+        openErrorMessage('Tasks must contain text. Please enter task again.');
+        
+        // Focus on add task field.
+        setFocusAddTaskField();
+        
+        return false;
+    }
+
+    // Create new list item and append user input.
     var li = document.createElement('LI');
-    var inputValue = document.getElementById('newTaskInput').value;
     var txt = document.createTextNode(inputValue);
     li.appendChild(txt);
 
-    // Toggle strikethrough on click for new list item.
+    // Toggle 'checked' class on click for new list item.
     txt.addEventListener('click', function(e) {
         e.target.classList.toggle('checked');
     });
     
-    // Append new item to task list.
+    // Append new list item to task list.
     document.querySelector('UL').appendChild(li);
     
-    // Clear add task field.
+    // Clear user input field.
     document.getElementById('newTaskInput').value = '';
 
     // Append close button and add remove node event to new item.
     var span = document.createElement('SPAN');
-    var txt = document.createTextNode(closeButton);
+    txt = document.createTextNode(closeButton);
     span.className = closeButtonClasses;
     span.appendChild(txt);
     span.addEventListener('click', function(e) {
