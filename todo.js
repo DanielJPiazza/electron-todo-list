dedbd301c140d85830304df8646976f14f826cf4
DanielJPiazza <daniel@example.com>
2019-12-09 17:16:03 -0500

    Refactor, whitespace, comments.

--- todo.js
+++ todo.js
@@ -1,19 +1,29 @@
 'use strict';
 
 
-// -- GLOBAL CONSTANTS --
 
+// ** GLOBAL CONSTS & VARS **
+
+// Requires
 const remote = require('electron').remote
-const closebuttonclasses = 'closeButton';
-const closeButton = '\u2716';
 const fs = require('fs');
+
+// For CSS & HTML
+const removeTaskButtonClass = 'removeTaskButton';
+const removeTaskButton = '\u2716';
+
+// Input/output
 var userDataArray = fs.readFileSync('userdata.txt').toString().split("\n");
 
+// Error modal handling
+const modal = document.querySelector('.modal');
+const modalCloseButton = document.querySelector('.modal-close-button');
 
-// -- UTILITY FUNCTIONS --
+
+
+// ** FUNCTIONS **
 
 function closeWindow() {
-    // Not used in todo.js. Called in index.html.
     remote.getCurrentWindow().close()
 }
 
@@ -25,11 +35,10 @@ function removeFocusAddTaskField() {
     document.getElementById('newTaskInput').blur();
 }
 
-// Called by createTaskOnSetup().
-function createTaskOnSetupUtility(data) {
+function createTask(data) {
     // Create new list item and append user input.
     var li = document.createElement('LI');
-    var txt = document.createTextNode(data.toString());
+    var txt = document.createTextNode(data);
     li.appendChild(txt);
 
     // Toggle 'checked' class on click for new list item.
@@ -40,10 +49,10 @@ function createTaskOnSetupUtility(data) {
     // Append new list item to task list.
     document.querySelector('UL').appendChild(li);
 
-    // Append close button and add remove node event to new item.
+    // Append remove task button and add remove node event to remove button.
     var span = document.createElement('SPAN');
-    txt = document.createTextNode(closeButton);
-    span.className = closebuttonclasses;
+    txt = document.createTextNode(removeTaskButton);
+    span.className = removeTaskButtonClass;
     span.appendChild(txt);
     span.addEventListener('click', function(e) {
         e.target.parentNode.remove();
@@ -55,47 +64,43 @@ function createTaskOnSetupUtility(data) {
 // Currently the file is 'userdata.txt' stored in the application root.
 function createTaskOnSetup(data) {
     if (data.length === 1 && data[0] === "") {    
-        // Add a default task if the user's file is empty.
-        createTaskOnSetupUtility("Create a new task or delete this one...");
+        // Add default task if the user's file is empty.
+        createTask("Create a new task or delete this one...");
     } else if (data.length === 1) {
-        // Avoid calling a for loop if only one task.
-        createTaskOnSetupUtility(data[0]);
+        // Avoid calling for loop if only one task.
+        createTask(data[0]);
     } else {
-        // Pass the full array if more than one task.
+        // Pass full array if more than one task.
         for (var i in data) {
-            createTaskOnSetupUtility(data[i]);
+            createTask(data[i]);
         }
     }
 }
 
+// Create a new list item when 'Add Task' is clicked.
+function createTaskFromForm() {
+    // Gather user input.
+    var inputValue = document.getElementById('newTaskInput').value.trim();
 
-// -- INITIAL SETUP --
-
-// Load user's saved data and populate task list.
-createTaskOnSetup(userDataArray);
-
-// Allow 'Enter' key to call createTaskFromForm().
-document.getElementById('newTaskInput').addEventListener('keypress', function(e) {
-    if (e.which === 13 || e.keyCode === 13) {
-      createTaskFromForm();
+    // Verify user input contains characters other than whitepsace.
+    var inputRegex = new RegExp('^\\S');
+    if (!inputRegex.test(inputValue)) {
+        // If check fails, display error message.
+        toggleModal('Tasks must contain text.');
+        return false;
     }
-}, false);
 
+    // Create task list item and add to DOM.
+    createTask(inputValue);
 
-// -- IPC COMMUNICATION WITH MAIN.JS --
+    // Clear user input field.
+    document.getElementById('newTaskInput').value = '';
+    
+    // Focus on add task field.
+    setFocusAddTaskField();
+}
 
-// Receive window size from main process when window is resized.
-// Set height of todo list accordingly to avoid main window scrollbar.
-require('electron').ipcRenderer.on('async-resize', (event, message) => {
-    document.getElementById('todoList').style.height = ((message - 220).toString() + 'px');
-});
-
-
-// -- ERROR MODAL HANDLING --
-
-const modal = document.querySelector('.modal');
-const modalCloseButton = document.querySelector('.modal-close-button');
-
+// Display error modal with the passed argument.
 function toggleModal(message) {     
     document.getElementById('inputErrorMessage').textContent = message;
     modal.classList.toggle('show-modal');
@@ -106,6 +111,30 @@ function toggleModal(message) {
         setFocusAddTaskField();
     }
 }
+
+
+
+// ** OTHER NON-FUNCTION LOGIC **
+
+// Receive window size from main process (IPC) when window is resized.
+// Set height of todo list accordingly to avoid main window scrollbar.
+require('electron').ipcRenderer.on('async-resize', (event, message) => {
+    document.getElementById('todoList').style.height = ((message - 220).toString() + 'px');
+});
+
+
+
+// ** INITIAL SETUP & NON-FUNCTION EVENT LISTENERS **
+
+// Load user's saved data and populate task list.
+createTaskOnSetup(userDataArray);
+
+// Allow 'Enter' key to call createTaskFromForm().
+document.getElementById('newTaskInput').addEventListener('keypress', function(e) {
+    if (e.which === 13 || e.keyCode === 13) {
+      createTaskFromForm();
+    }
+}, false);
 
 // Allow mouse click on '.modal-close-button' to close error modal.
 modalCloseButton.addEventListener('click', function(e) {
@@ -125,49 +154,3 @@ document.addEventListener('keydown', function(e) {
         toggleModal(null);
     }
 });
-
-
-// -- NEW TASK FUNCTION --
-
-// Create a new list item when 'Add Task' is clicked.
-function createTaskFromForm() {
-    // Gather user input.
-    var inputValue = document.getElementById('newTaskInput').value.trim();
-
-    // Verify user input contains characters other than whitepsace.
-    var inputRegex = new RegExp('^\\S');
-    if (!inputRegex.test(inputValue)) {
-        // If check fails, display error message.
-        toggleModal('Tasks must contain text.');
-        return false;
-    }
-
-    // Create new list item and append user input.
-    var li = document.createElement('LI');
-    var txt = document.createTextNode(inputValue);
-    li.appendChild(txt);
-
-    // Toggle 'checked' class on click for new list item.
-    li.addEventListener('click', function(e) {
-        e.target.classList.toggle('checked');
-    });
-    
-    // Append new list item to task list.
-    document.querySelector('UL').appendChild(li);
-    
-    // Clear user input field.
-    document.getElementById('newTaskInput').value = '';
-
-    // Append close button and add remove node event to new item.
-    var span = document.createElement('SPAN');
-    txt = document.createTextNode(closeButton);
-    span.className = closebuttonclasses;
-    span.appendChild(txt);
-    span.addEventListener('click', function(e) {
-        e.target.parentNode.remove();
-    });
-    li.appendChild(span);
-
-    // Focus on add task field.
-    setFocusAddTaskField();
-}
